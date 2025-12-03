ac76d5f61a7327805f36a3f878365f9fdf7ed029
millyjain09 <millyjain88@gmail.com>
2025-12-03 11:27:29 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,10 @@ app.use("/api/message", messageRoutes);
 
 // Deployment
 const __dirname1 = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
@@ -43,6 +45,7 @@ if (process.env.NODE_ENV === "production") {
     res.send("API is running..");
   });
 }
+
 
 // Error Handling
 app.use(notFound);
